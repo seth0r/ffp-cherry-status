7a6b20a754eb7888ddf6e7bdb3d35524a078f5ae
root <root@v220230257037219818.happysrv.de>
2023-06-03 15:48:10 +0200

    added nodeinfo

--- modules/nodemap.py
+++ modules/nodemap.py
@@ -2,29 +2,42 @@ import cherrypy
 from cherrypy._cperror import HTTPRedirect
 from cherrypy.lib.static import serve_fileobj, serve_file
 import os
+import time
+import math
 import random
 import json
 
+def getnodeloc(node):
+    if "location" in node:
+        return node["location"]
+    if "location_guess" in node:
+        return node["location_guess"]
+    random.seed(node["_id"])
+    return [
+        float(os.getenv("DEFLON","0")) - 0.001 + random.random() * 0.002,
+        float(os.getenv("DEFLAT","0")) - 0.001 + random.random() * 0.002,
+    ]
+
 class NodeMap:
     def node2gjs(self,node):
-        if "location" not in node and "location_guess" in node:
-            node["location"] = node["location_guess"]
-        if "location" not in node:
-            random.seed(node["_id"])
-            node["location"] = [
-                float(os.getenv("DEFLON","0")) - 0.001 + random.random() * 0.002,
-                float(os.getenv("DEFLAT","0")) - 0.001 + random.random() * 0.002,
-            ]
+        now = time.time()
+        loc = getnodeloc(node)
+        nexthop = self.mdb["nodes"].find_one({ "ifaddr":node.get("network",{}).get("nexthop",None) })
+        color = ([ co[0] for co in sorted( filter(
+                lambda co: node['offline'] <= co[1],
+                node.get("offline_limits",{}).items()
+            ), key = lambda co: co[1] )]+[None] )[0]
         f = {
             "type": "Feature",
             "geometry": {
                 "type": "Point",
-                "coordinates": node["location"],
+                "coordinates": loc,
             },
             "properties": {
-                "node_id": node["_id"],
-                "popup": self.get_tpl( "map/node_popup.html" ).render( node = node ),
-                "info":  self.get_tpl( "map/node_info.html"  ).render( node = node ),
+                "type" : "node",
+                "id"   : node["_id"],
+                "popup": self.get_tpl( "map/node_popup.html" ).render( node = node, nexthop = nexthop, loc = loc, color = color, now = now ),
+                "info" : self.get_tpl( "map/node_info.html"  ).render( node = node, nexthop = nexthop, loc = loc, color = color, now = now ),
             }
         }
         vpn = node.get("network",{}).get("mesh_vpn",{})
@@ -33,19 +46,26 @@ class NodeMap:
             f["properties"][a] = node[a]
         return f
 
+    def nodeaddsettings(self,node):
+        if node is None:
+            return
+        for i in ["",node["_id"]]:
+            ns = self.mdb["node_settings"].find_one({"_id":i})
+            if ns is not None:
+                ns.pop("_id",None)
+                node.update(ns)
+        return node
+
     @cherrypy.expose
     def nodes_geojson(self):
         gjs = { "type": "FeatureCollection","features": [] }
         
-        nodes = self.mdb["nodes"].find( {}, sort = [("last_ts",-1)] )
-        for n in nodes:
-            for i in [n["_id"],""]:
-                ns = self.mdb["node_settings"].find_one({"_id":i})
-                if ns is not None:
-                    ns.pop("_id",None)
-                    n.update(ns)
-                    break
-            gjs["features"].append( self.node2gjs(n) )
+        for n in self.mdb["nodes"].find( {}, sort = [("last_ts",-1)] ):
+            gjs["features"].append( self.node2gjs( self.nodeaddsettings(n) ) )
+
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        return bytes(json.dumps(gjs),"utf-8")
+
 
         cherrypy.response.headers['Content-Type'] = 'application/json'
         return bytes(json.dumps(gjs),"utf-8")
---
+++ tpl/helpers.tpl
@@ -0,0 +1,27 @@
+{% macro formatDuration( s, r=1 ) %}
+    {% set s = s // r * r %}
+    {% set d = s // (24 * 60 * 60) %}
+    {% set s = s % (24 * 60 * 60) %}
+    {% set h = s // (60 * 60) %}
+    {% set s = s % (60 * 60) %}
+    {% set m = s // 60 %}
+    {% set s = s % 60 %}
+    {{ "%dd" % d if d > 0 else '' }}
+    {{ "%dh" % h if h > 0 else '' }}
+    {{ "%dm" % m if m > 0 else '' }}
+    {{ "%ds" % s if s > 0 else '' }}
+{% endmacro %}
+
+{% macro formatMeters( m ) %}
+    {% if m > 1000 %}
+        {{ (m / 1000)|round(1) }} km
+    {% else %}
+        {{ m|int }} m
+    {% endif %}
+{% endmacro %}
+
+{% macro jsClock( now, start ) %}
+<span class="value">{{ formatDuration( start ) }}</span>
+<span class="now">{{ now }}</span>
+<span class="start">{{ start }}</span>
+{% endmacro %}
